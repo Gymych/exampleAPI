f8e014d7724bc81abeab00308e1a8e20cda5697b
Gymych <gymych@gmail.com>
2022-04-10 11:15:08 +0300

    Models, migrations, controllers created

---
+++ database/migrations/2022_04_10_075508_create_item_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('item_categories', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('item_id');
+            $table->unsignedBigInteger('category_id');
+            $table->index('item_id', 'item_category_item_idx');
+            $table->index('category_id', 'item_category_category_idx');
+            $table->foreign('item_id', 'item_category_item_fk')->on('items')->references('id');
+            $table->foreign('category_id', 'item_category_category_fk')->on('categories')->references('id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('item_categories');
+    }
+};
