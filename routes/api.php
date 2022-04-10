1882bb7e6c8edd6f78b5212add2429f33fe2557e
Gymych <gymych@gmail.com>
2022-04-10 12:02:37 +0300

    Routes

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::prefix('v1')->group(function () {
+    Route::group(['namespace' => '\App\Http\Controllers\V1\Item'], function() {
+        Route::post('item/create', 'CreateController');
+        Route::patch('item/{item}/edit', 'EditController');
+        Route::delete('item/{item}/delete', 'DeleteController');
+        Route::get('item', 'ShowController');
+    });
+    Route::group(['namespace' => '\App\Http\Controllers\V1\Category'], function() {
+        Route::post('category/create', 'CreateController');
+        Route::delete('category/{category}/delete', 'DeleteController');
+    });
+});
